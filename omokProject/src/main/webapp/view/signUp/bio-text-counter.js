c71dfb6f9e66fe29c377fbbca9d0018cf8df47b3
sunJ0120 <sspure1214@gmail.com>
2025-05-24 18:25:13 +0900

    Refactor: js코드 DOM Listner로 묶어서 문서 로드 후 안전하게 실행되도록 코드 리팩토링

--- omokProject/src/main/webapp/view/signUp/bio-text-counter.js
+++ omokProject/src/main/webapp/view/signUp/bio-text-counter.js
@@ -1,6 +1,8 @@
-const bio = document.getElementById('bio');
-const counter = document.getElementById('bio_counter');
+document.addEventListener('DOMContentLoaded', () => {
+    const bio = document.getElementById('bio');
+    const counter = document.getElementById('bio_counter');
 
-bio.addEventListener('input', () => {
-    counter.textContent = `${bio.value.length} / 60`;
+    bio.addEventListener('input', () => {
+        counter.textContent = `${bio.value.length} / 60`;
+    });
 });
--- omokProject/src/main/webapp/view/signUp/profile-random.js
+++ omokProject/src/main/webapp/view/signUp/profile-random.js
@@ -1,5 +1,11 @@
-document.querySelector(".avatar-random-btn").addEventListener("click", () => {
-    const randomNum = Math.floor(Math.random() * 6) + 10; // 임시로 10~15까지
-    document.querySelector("#profile").src = `../../img/profile/${randomNum}.png`;
-    document.querySelector("#profileNumber").value = randomNum;
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".avatar-random-btn");
+    const profileImg = document.querySelector("#profile");
+    const profileInput = document.querySelector("#profileNumber");
+
+    button.addEventListener("click", () => {
+        const randomNum = Math.floor(Math.random() * 6) + 10; // 10~15까지
+        profileImg.src = `../../img/profile/${randomNum}.png`;
+        profileInput.value = randomNum;
+    });
 });

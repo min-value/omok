5fca3350fa06c9c7180d1da2146019ae45159698
subin930 <bread0930@g.skku.edu>
2025-05-27 19:02:27 +0900

    Feat: MainDAO/Service/Controller 추가, JSP로 변환, sad 이미지 추가

--- omokProject/src/main/webapp/view/home/home_script.js
+++ omokProject/src/main/webapp/view/home/home_script.js
@@ -1,39 +1,17 @@
+const pencilSrc = `/img/pencil_icon.png`;
+const checkSrc = `/img/check_icon.png`;
+let editing = false; //bio 수정 상태 flag
+
 $(window).ready(function(){
-    const pencilSrc = "../../img/pencil_icon.png";
-    const checkSrc = "../../img/check_icon.png";
+    let normalImgUrl = "/img/profile/" + imageNum + ".png";
+    let sadImgUrl = "/img/profile/" + imageNum + "_sad.png";
 
-    let editing = false;
-    //DB 연결 후 AJAX로 처리
-    setProfile("bread0930", "일이삼사오륙칠팔구십일이삼사오륙칠팔구십", 10, 3, 1);
-    setBar(55);
-
+    /* 한줄 소개 변경 클릭 리스너*/
     $('#edit_icon').on('click', function() {
-        let bio_text = $('.bio_text');
-        if(!editing) {
-            editing = true;
-            bio_text.focus();
-            bio_text
-                .prop('readonly', false);
-            setBioBorder(editing);
-            $(this)
-                .attr('src', checkSrc);
-        } else {
-            editing = false;
-            bio_text
-                .prop('readonly', true);
-            setBioBorder(editing);
-            $(this)
-                .attr('src', pencilSrc);
-            let newBio = bio_text
-                .val();
-
-            alert('변경 완료하였습니다.');
-            console.log(newBio);
-
-            //db에 반영
-        }
+        updateBio();
     })
 
+    /* 한줄 소개 박스 포커스 리스너*/
     $('textarea.bio_text').on({
         focus: function () {
             setBioBorder(editing);
@@ -48,19 +26,48 @@ $(window).ready(function(){
         }
     });
 
+    /* 로그아웃 버튼 호버 리스너*/
     $('#logout-btn img').hover(
         function() {
             $('#avatar')
-                .css('background-image', 'url("../../img/profile/2_sad.png")')
+                .css('background-image', 'url(' + sadImgUrl + ')');
         },
         function() {
             $('#avatar')
-                .css('background-image', 'url("../../img/profile/2.png")');
+                .css('background-image', 'url(' + normalImgUrl + ')');
         }
     );
 
 });
 
+/* 한줄 소개 변경 함수 */
+function updateBio() {
+    let bio_text = $('.bio_text');
+    if(!editing) {
+        editing = true;
+        bio_text.focus();
+        bio_text
+            .prop('readonly', false);
+        setBioBorder(editing);
+        $(this)
+            .attr('src', checkSrc);
+    } else {
+        editing = false;
+        bio_text
+            .prop('readonly', true);
+        setBioBorder(editing);
+        $(this)
+            .attr('src', pencilSrc);
+        let newBio = bio_text
+            .val();
+
+        alert('변경 완료하였습니다.');
+        console.log(newBio);
+
+        //db에 반영
+    }
+}
+/* 한줄 소개 박스 focus에 따른 보더 설정 함수 */
 function setBioBorder(editing) {
     if(editing) {
         $('.value.bio').css('border', '2px solid #207600');
@@ -68,6 +75,7 @@ function setBioBorder(editing) {
         $('.value.bio').css('border', 'none');
     }
 }
+
 /* 개인정보 업데이트 함수 */
 function setProfile(id, bio, win, lose, image_num) {
     $('.value.id')
@@ -77,7 +85,7 @@ function setProfile(id, bio, win, lose, image_num) {
     $('.value-winning')
         .text((win + lose) + '전 ' + win + '승 ' + lose + '패');
     $('#avatar')
-        .css('background-image', 'url(../../img/profile/' + image_num + '.png');
+        .css('background-image', 'url(/img/profile/' + image_num + '.png');
 }
 
 /* 그래프 업데이트 함수 */
@@ -94,4 +102,9 @@ function setBar(winRate) {
 
 /* 마이 랭킹 업데이트 함수 */
 function setMyRanking(rank, id, winRate) {
+    let my_rank = $('#my_rank');
+
+    //등수 업데이트
+
+
 }
